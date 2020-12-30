d236e0e80e4d8f0dbd6ba661e32bb96a7acdf03e
Chris Bowden <chris.bowden@gnl-x-l-0003.pt.playtech.corp>
2020-12-30 18:22:31 +0000

    ch06: error handling

---
+++ ch06-error-handling-and-performance/api/weather_api.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+import fastapi
+from fastapi import Depends
+
+from models.location import Location
+from models.validation_error import ValidationError
+from services import openweather_service
+
+router = fastapi.APIRouter()
+
+
+@router.get('/api/weather/{city}')
+async def weather(
+	loc: Location = Depends(),
+	units: Optional[str] = 'metric'
+):
+	try:
+		return await openweather_service.get_report_async(loc.city, loc.state, loc.country, units)
+	except ValidationError as ve:
+		return fastapi.Response(content=ve.error_msg, status_code=ve.status_code)
+	except Exception as e:
+		return fastapi.Response(content=str(e), status_code=500)
